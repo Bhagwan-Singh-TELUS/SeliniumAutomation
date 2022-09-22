e9cf17ed725d003fd9c8f119dcdab86f5ca5f9e0
Bhagwan <112369112+Bhagwan-Singh-TELUS@users.noreply.github.com>
2022-09-22 17:54:23 +0530

    Update RegisterDemoEndToEnd.java

--- Selinium Automation/src/com/telus/git/seliniumautomation/RegisterDemoEndToEnd.java
+++ Selinium Automation/src/com/telus/git/seliniumautomation/RegisterDemoEndToEnd.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 // Program Update By Bhagwan Singh 07 Sep 2022 In GitHub
+// Update on 22 sept 2022
 
 
 public class RegisterDemoEndToEnd {
